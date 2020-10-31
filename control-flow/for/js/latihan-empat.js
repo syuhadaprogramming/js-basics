5ab98958d51215bfa72b68d6700b58c56ec108f1
syuhadaprogramming <syuhadaprogramming@gmail.com>
2020-10-31 20:09:13 +0700

    finish code latihan empat.js

--- control-flow/for/js/latihan-empat.js
+++ control-flow/for/js/latihan-empat.js
@@ -1,3 +1,23 @@
 // change judul index.html
 const judul = document.querySelector("h2");
 judul.innerHTML = "Latihan 4 - Demerit Points";
+
+function checkSpeed(speed) {
+  // variable
+  const speedLimit = 70;
+  const kmPerPoints = 5;
+
+  // logic
+  if (speed < speedLimit + kmPerPoints) {
+    console.log("Ok");
+  } else {
+    const point = Math.floor((speed - speedLimit) / kmPerPoints);
+    if (point >= 12) {
+      console.log("License suspended");
+    } else {
+      console.log("Point : ", point);
+    }
+  }
+}
+
+checkSpeed(129);
