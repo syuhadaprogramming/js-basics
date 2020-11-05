e14f20da48990feaa05eb8fc291f4c8e6f54cf52
syuhadaprogramming <syuhadaprogramming@gmail.com>
2020-11-05 12:50:43 +0700

    add file code materi mosh tentang arrays dan object

---
+++ object/1. object/js/script.js
@@ -0,0 +1,11 @@
+const keluargaSyuhada = {
+  suami: "Syuhada",
+  istri: "Desi",
+  anak: "Hamzah Syuhada",
+  nikah: function () {
+    console.log("Hasil pernikahan Syuhada dan Desi adalah Hamzah Syuhada");
+  },
+  masihMenikah: true,
+};
+
+console.log(keluargaSyuhada.masihMenikah);
