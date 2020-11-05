e99dfd39a5acab87fe73640133775660e25ebcce
syuhadaprogramming <syuhadaprogramming@gmail.com>
2020-11-05 13:13:33 +0700

    materi factory functions selesai

--- object/2. factory-functions/js/script.js
+++ object/2. factory-functions/js/script.js
@@ -1,12 +1,24 @@
-const keluargaSyuhada = {
-    suami: "Syuhada",
-    istri: "Desi",
-    anak: "Hamzah Syuhada",
-    nikah: function () {
-      console.log("Hasil pernikahan Syuhada dan Desi adalah Hamzah Syuhada");
+// function keluarga
+function keluarga(suami, istri, anak) {
+  return {
+    // if key and value sama maka bisa ditulis satu properti saja
+    suami,
+    istri,
+    anak,
+    nikah() {
+      console.log(`Hasil pernikahan ${suami} dan ${istri} adalah ${anak}`);
     },
-    masihMenikah: true,
+    statusMenikah: true,
   };
-  
-  console.log(keluargaSyuhada.masihMenikah);
-  
+}
+
+// define
+let keluargaSatu = keluarga("Syuhada", "Desi", "Hamzah Syuhada");
+let keluargaDua = keluarga("Mista", "Sani", "Encih,Syuhada,Yati,Yuli,Adung");
+
+// cetak
+console.log(keluargaSatu);
+console.log(keluargaSatu.nikah());
+
+console.log(keluargaDua);
+console.log(keluargaDua.nikah());
